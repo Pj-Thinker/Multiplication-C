47dbdb3fc3d5854dc0ce3e28176957a7aa62d022
Pejman Naderi <pj.naderi@gmail.com>
2020-03-18 16:30:10 +0330

    Input numbers
    Useing sample numbers or inserting arbitrary numbers.

--- main.c
+++ main.c
@@ -34,5 +34,29 @@ int main()
 
 void multiplication()
 {
+    // sample numbers
+    char n1[] = "91111111111111111111111111111111111111111111111112";
+    char n2[] = "00000000000000000000000000000000000000000000000012";
+
+    printf("\n-----------------Sample numbers-----------------\n");
+    printf("%s\n", n1);
+    printf("%s\n", n2);
+    printf("\n------------------------------------------------\n");
+
+    // use sample numbers or input numbers as string
+    char choice;
+    printf("Do you want to use sample numbers?\n"
+           "~~~> Yes: y (or any letter)\n"
+           "~~~> No: n\nYour choice:");
+    scanf(" %c", choice);
+
+    // enter numbers that you want
+    if (choice == 'n')
+    {
+        printf("Please enter the first number:\n");
+        scanf("%s", n1);
+        printf("Please enter the second number:\n");
+        scanf("%s", n2);
+    }
 
 }
